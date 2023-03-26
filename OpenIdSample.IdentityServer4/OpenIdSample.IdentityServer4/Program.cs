249984e2d416d1cb6d59b1b2f6773bf154eef68c
RichZHY <HUANYUAN.ZHANG@GMAIL.COM>
2023-03-26 11:06:52 +1100

    Added IdentityServer4 and WebAPI

--- OpenIdSample.IdentityServer4/OpenIdSample.IdentityServer4/Program.cs
+++ OpenIdSample.IdentityServer4/OpenIdSample.IdentityServer4/Program.cs
@@ -1,6 +1,29 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using OpenIdSample.IdentityServer4.IdentityConfiguration;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(Clients.Get())
+    .AddInMemoryIdentityResources(Resources.GetIdentityResources())
+    .AddInMemoryApiResources(Resources.GetApiResources())
+    .AddInMemoryApiScopes(Scopes.GetApiScopes())
+    .AddTestUsers(Users.Get())
+    .AddDeveloperSigningCredential();
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseIdentityServer();
+
+app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
